3c7d0e274a2d36f271fab5e6b8e25be2749d6d88
Bukaii <lyanosongbo8@gmail.com>
2025-08-26 01:54:03 -0300

    se añadió el puerto

--- laravel/config/db.php
+++ laravel/config/db.php
@@ -1,10 +1,11 @@
 <?php
 $host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
 $db   = getenv('DB_NAME');
+$pass = getenv('DB_PASS');
+$user = getenv('DB_USER');
+$port = getenv('DB_PORT');
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
