2bb6c865784593234fcd9542ca36bf12fcd8aa75
Bukaii <lyanosongbo8@gmail.com>
2025-09-04 02:06:58 -0300

    eaeaeae

--- laravel/config/db.php
+++ laravel/config/db.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
 $host = getenv('DB_HOST');
 $db   = getenv('DB_NAME');
 $pass = getenv('DB_PASS');
