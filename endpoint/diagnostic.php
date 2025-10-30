2de8419fee4ba7bbc7cff5990c066253de44d640
Bukaii <lyanosongbo8@gmail.com>
2025-10-29 21:19:31 -0300

    sacar los sh ineccesarios

--- endpoint/diagnostic.php
+++ endpoint/diagnostic.php
@@ -1,5 +1,7 @@
 <?php
-header('Content-Type: text/plain; charset=UTF-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
 
 function fail($msg) {
     http_response_code(500);
