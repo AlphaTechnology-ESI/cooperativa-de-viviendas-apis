dcbd0e1ed28611665eaf6a81263365c1bd098faf
Bukaii <lyanosongbo8@gmail.com>
2025-11-01 22:44:59 -0300

    comprobar valor host

--- config/db.php
+++ config/db.php
@@ -23,6 +23,7 @@ if (file_exists(__DIR__ . '/db_externa.php')) {
 $conn = new mysqli($host, $user, $pass, "", $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    die("Valor de host: " . $host);
 }
 
 // Leer archivo SQL
